e5e91e3ed6b927a36999be7394cf6f27a5b7835d
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-05-04 18:45:02 -0300

    If there's no PHP files to lint, do not lint.

--- worker/linter/php/lint.sh
+++ worker/linter/php/lint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+PHPFILES=$(find . -type d -name "node_modules" -prune -o -type f -print | grep -Ec '\.(php)$')
+
+if [ "$PHPFILES" -eq 0 ]; then
+	echo "nothing to lint here";
+	return 0;
+fi
+
 phpcs --runtime-set ignore_warnings_on_exit 1 --runtime-set ignore_errors_on_exit 1 --extensions="php" --ignore=*/node_modules/* --report-json="${LINT_JSON}" "${GITHUB_WORKSPACE}" || LINT_EXIT_CODE=$?
 
