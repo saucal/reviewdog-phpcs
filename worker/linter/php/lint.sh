bdf783312b8b6a092ef85047775ffffdc5795538
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-05-04 18:39:02 -0300

    Errors and warnings don't make phpcs fail.
    Exit codes are not greatly documented right now.

--- worker/linter/php/lint.sh
+++ worker/linter/php/lint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-phpcs --extensions="php" --ignore=*/node_modules/* --report-json="${LINT_JSON}" "${GITHUB_WORKSPACE}" || LINT_EXIT_CODE=$?
+phpcs --runtime-set ignore_warnings_on_exit 1 --runtime-set ignore_errors_on_exit 1 --extensions="php" --ignore=*/node_modules/* --report-json="${LINT_JSON}" "${GITHUB_WORKSPACE}" || LINT_EXIT_CODE=$?
 
